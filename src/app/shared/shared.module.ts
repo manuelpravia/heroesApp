dd7ebb9f354db2875c40e2a6d2ec6bea5f005a3b
manuelpravia <praviapuri@gmail.com>
2023-12-23 22:29:36 -0500

    primera version: configuracion de rutas

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    NotFoundPageComponent
+  ],
+  exports: [
+    NotFoundPageComponent
+  ]
+})
+export class SharedModule { }
