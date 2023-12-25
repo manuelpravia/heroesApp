224af9600ad23816bb47f69a3ba3649ac2770e7f
manuelpravia <praviapuri@gmail.com>
2023-12-25 02:33:54 -0500

    cuarta version: proteccion de rutas

---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, CanMatch, Route, UrlSegment, RouterStateSnapshot, UrlTree, Router } from "@angular/router";
+import { Observable, tap } from "rxjs";
+import { AuthService } from "../services/auth.service";
+
+
+@Injectable({providedIn: 'root'})
+export class AuthGuard implements CanMatch, CanActivate{
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) {}
+
+  private chekAuthStatus(): boolean | Observable<boolean>{
+
+      return this.authService.checkAuthentication()
+      .pipe(
+        tap(isAuthenticated => {
+          if ( !isAuthenticated ) {
+            this.router.navigate(['./auth/login'])
+          }
+        })
+      );
+  }
+
+  canMatch(route: Route, segments: UrlSegment[]): boolean | Observable<boolean> {
+    //console.log('Can Match')
+    //console.log({route,segments})
+    return this.chekAuthStatus();
+  }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
+    //console.log('Can Activated')
+   // console.log({route, state});
+    return this.chekAuthStatus();;
+  }
+
+}
---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { environments } from "src/environments/environments";
+import { User } from "../interfaces/user.interface";
+import { Observable, catchError, map, of, tap } from 'rxjs';
+
+
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+
+  private baseUrl = environments.baseUrl;
+  private user?: User;
+
+  constructor(private http: HttpClient){}
+
+  get currentUser(): User|undefined {
+    if( !this.user ) return undefined;
+    return structuredClone( this.user );
+
+  }
+
+  login( email: string, password: string): Observable<User>{
+    // http.post('login',{email,password})
+
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+    .pipe(
+      tap(user => this.user = user ),
+      tap(user => localStorage.setItem('token', 'Ashdbg.f54sgsdjhgfh.5fh5df')),
+    );
+
+  }
+
+  logout () {
+    this.user = undefined;
+    localStorage.clear();
+  }
+
+  checkAuthentication(): Observable<boolean>{
+
+    if ( !localStorage.getItem('token')) return of(false);
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+    .pipe(
+      tap( user => this.user = user),
+      map(user => !!user),
+      catchError(  err => of(false))
+    );
+  }
+
+
+}
+
+
